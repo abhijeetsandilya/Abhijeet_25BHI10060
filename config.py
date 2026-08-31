cf758fb4e2928bac7864e047a0b979f8af94b899
abhijeetsandilya <abhijeetsandilya8@gmail.com>
2026-08-31 23:59:26 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+LLM_MODEL = "llama-3.3-70b-versatile"
+
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
+
+if not GROQ_API_KEY:
+
+    raise ValueError(
+    "GROQ_API_KEY not found. Please add it to your .env file."
+    )
